422cb4fb1a908a0c800ce6d96227b5b3c79438c3
Effie28 <efdau@ulaval.ca>
2024-07-10 21:30:07 -0400

    first working version of the pipeline

--- gps_calcul_pos/gps_compute.py
+++ gps_calcul_pos/gps_compute.py
@@ -11,7 +11,6 @@ from tf2_ros import TransformBroadcaster
 
 
 class Gps_compute(Node):
-
     def __init__(self):
         super().__init__('gps_compute')
         input_topic = self.declare_parameter('input_topic', '/gps_value').value
@@ -41,19 +40,28 @@ class Gps_compute(Node):
         # x[0], y[0] = convertion2949.transform(gps_values[0], gps_values[1])
         # x[1], y[1] = convertion2949.transform(gps_values[3], gps_values[4])
         # x[2], y[2] = convertion2949.transform(gps_values[6], gps_values[7])
-        x = [0, 0 ,0]
-        y = [0, 0 ,0]
-        z = [0, 0 ,0]
+        self.pose = PoseStamped()
+        self.pose.header.frame_id = 'world'
+        
+        #TODO: find a way to get the timestamp from the gps message
+        # self.pose.header.stamp.sec = msg.header.stamp.sec
+        # self.pose.header.stamp.nanosec = msg.header.stamp.nanosec
+        
         gps_values = list(msg.data)
+        x = [0, 0, 0]
+        y = [0, 0, 0]
+        z = [0, 0, 0]
         convertion2955 = Transformer.from_crs("EPSG:4326", "EPSG:2955", always_xy=True)
-
-        x[0], y[0], z[0] = convertion2955.transform(gps_values[0], gps_values[1], gps_values[2])
-        x[1], y[1], z[1] = convertion2955.transform(gps_values[3], gps_values[4], gps_values[5])
-        x[2], y[2], z[2] = convertion2955.transform(gps_values[6], gps_values[7], gps_values[8])
-        position = np.array([x, y, z])
-        self.compute_calibration(position)
-        if self.compute_calibration:
-            self.publish_pose(position)
+        if gps_values is not None:
+            for i in range(3):
+                x[i], y[i], z[i] = convertion2955.transform(gps_values[i*3], gps_values[i*3+1], gps_values[i*3+2])
+            position = np.array([x, y, z])
+            # self.get_logger().info(f"Received GPS values: {position}")
+            self.compute_calibration(position)
+            # self.get_logger().info(f"Calibration computed!")
+            self.publish_transform(self.T_world_to_base_link)
+            self.publish_pose(self.T_world_to_base_link)
+            # self.get_logger().info(f"Transofrmation matrix: {self.T_world_to_base_link}")
             
     def tf_callback(self, tf_msg):
         for tf in tf_msg.transforms:
@@ -70,38 +78,39 @@ class Gps_compute(Node):
             self.tf_acquired = True
             self.destroy_subscription(self.tf_sub)
     
-    def compute_calibration(self, position):
+    def compute_calibration(self, P):
+        #TODO : uncomment those lines when Q is in URDF
         # while self.tf_acquired == False:
         #     self.get_logger().info("Waiting for tf transform...")
         #     time.sleep(0.5)
-        P = np.array([position, [1,1,1]])#P est une matrice qui provient de valeur mesuré par le robot 
-        Q = np.array([[2.980048, -0.249529, -0.680838], [3.753882, -0.211299, -0.584590], [3.611486,  0.286080, -0.561145], [1,1,1]]) #Q est une matrice qui provient de valeur mesure au lab 
         # Q = np.array([self.Q1, self.Q2, self.Q3]).T
-        # Q = np.vstack((Q, np.ones((1, Q.shape[1]))))
-
+        # Q = np.vstack((Q, np.ones((1, Q.shape[1])))) 
+        
+        Q = np.array([[2.980048, -0.249529, -0.680838], [3.753882, -0.211299, -0.584590], [3.611486,  0.286080, -0.561145], [1,1,1]]) #Q est une matrice qui provient de valeur mesure au lab 
+        P = np.vstack((P, np.ones((1, P.shape[1]))))#P est une matrice qui provient de valeur mesuré par le robot 
         if P is not None and Q is not None:
             self.T_world_to_base_link = self.minimization(P, Q)
-            self.get_logger().info(f"calibration computed: {self.T_world_to_base_link}")
+            # self.get_logger().info(f"GPS values: {P}")
         else:
-            self.get_logger().info("bad gps signal")
+            self.get_logger().info("Bad gps signal!")
                    
     def minimization(self, P, Q):
-        errors_before = Q - P # Errors at the beginning
-        mu_p = np.mean(P[0:3, :], axis=1) #Centroide of each pointcloud
+        errors_before = Q - P
+        mu_p = np.mean(P[0:3, :], axis=1)
         mu_q = np.mean(Q[0:3, :], axis=1)
-        P_mu = np.ones((3, P.shape[1])) # Centered each pointclouds
+        P_mu = np.ones((3, P.shape[1]))
         Q_mu = np.ones((3, Q.shape[1]))
         for i in range(0, P_mu.shape[1]):
             P_mu[0:3, i] = P[0:3, i] - mu_p
         for i in range(0, Q_mu.shape[1]):
             Q_mu[0:3, i] = Q[0:3, i] - mu_q
-        H = P_mu @ Q_mu.T # Cross covariance matrix
-        U, s, V = np.linalg.svd(H) # Use SVD decomposition
-        M = np.eye(3) # Compute rotation
+        H = P_mu @ Q_mu.T
+        U, s, V = np.linalg.svd(H)
+        M = np.eye(3)
         M[2, 2] = np.linalg.det(V.T @ U.T)
         R = V.T @ M @ U.T
-        t = mu_q - R @ mu_p # Compute translation
-        T = np.eye(4) # Transformation matrix
+        t = mu_q - R @ mu_p
+        T = np.eye(4)
         T[0:3, 0:3] = R
         T[0:3, 3] = t
         return T
@@ -109,8 +118,8 @@ class Gps_compute(Node):
     def publish_transform(self, T):
         t = TransformStamped()
         t.header.stamp = self.get_clock().now().to_msg()
-        t.header.frame_id = 'map'
-        t.child_frame_id = 'theodolite'
+        t.header.frame_id = 'base_link'
+        t.child_frame_id = 'world'
         t.transform.translation.x = T[0, 3]
         t.transform.translation.y = T[1, 3]
         t.transform.translation.z = T[2, 3]
@@ -129,8 +138,8 @@ class Gps_compute(Node):
         self.get_logger().info("Publishing calibration transform...")
         self.publish_transform(self.T_world_to_base_link)
     
-    def publish_pose(self, position):
-        position = np.array([position[0], position[1], position[2], 1])
+    def publish_pose(self, T):
+        position = T[0:3, 3]
         self.pose.pose.position.x = position[0]
         self.pose.pose.position.y = position[1]
         self.pose.pose.position.z = position[2]
@@ -141,9 +150,6 @@ def main(args=None):
     rclpy.init(args=args)
     gps_compute = Gps_compute()
     rclpy.spin(gps_compute)
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
     gps_compute.destroy_node()
     rclpy.shutdown()
 
